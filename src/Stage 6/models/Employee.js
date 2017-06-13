1ceb1d69d4cdf6a0e3868fa8baf8c6abf149385d
loren pabst <loren pabst>
2017-06-13 14:32:44 -0600

    stage 6 done

--- src/Stage 6/models/Employee.js
+++ src/Stage 6/models/Employee.js
@@ -1,9 +1,20 @@
 export default class Employee {
   // constructor
-  
-  // updateName
+  constructor(id, name, title, phone){
+    this.id = id;
+    this.name = name;
+    this.title = title;
+    this.phone = phone;
+  }
+  updateName(name){
+    this.name = name;
+  }
 
-  // updatePhone
+  updatePhone(phone){
+    this.phone = phone;
+  }
 
-   // updateTitle
+  updateTitle(title){
+    this.title = title;
+  }
 }
