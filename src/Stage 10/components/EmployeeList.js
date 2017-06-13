15ec0575ccd8c04cc560bf6e55799982e5d818fd
loren pabst <loren pabst>
2017-06-13 16:04:31 -0600

    built most of stage 10 from scratch

---
+++ src/Stage 10/components/EmployeeList.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+
+export default class EmployeeList extends Component{
+    render(){
+        return(
+            <div>
+                <ul className="listContainer">
+                { 
+                    this.props.employees.map((employee) => {
+                    return (
+                        <li className="listText" 
+                        key={employee.id} 
+                        onClick={ () => { this.props.selectEmployee(employee) }}
+                        > { employee.name } </li>
+                    )
+                    })
+                }
+                </ul>
+            </div>
+        );
+    }
+}
